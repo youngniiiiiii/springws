07e77bcfcde4ddc69ea880855ffa3ac83c14cbe7
youngeunkwon <youngnikk4929@gmail.com>
2023-04-26 17:43:37 +0900

    clone coding

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,37 @@
+package com.kbstar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String main() {
+        return "index";
+    }
+
+    @RequestMapping("/singlepost")
+    public String singlepost(Model model) {
+        model.addAttribute("center", "singlepost");
+        return "index";
+    }
+
+    @RequestMapping("/category")
+    public String category(Model model) {
+        model.addAttribute("center", "category");
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("center", "about");
+        return "index";
+    }
+    @RequestMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("center", "contact");
+        return "index";
+    }
+}
+
---
+++ src/test/java/com/kbstar/Clone0427ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clone0427ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
